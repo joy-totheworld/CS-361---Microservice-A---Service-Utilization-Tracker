a07a04ea153c783a4c4c370ac80fbe7880626de1
Joy <146790408+joy-totheworld@users.noreply.github.com>
2024-08-08 12:52:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -17,6 +17,91 @@ jsonTaskManagerUsedReq = {"sender": "task-manager", "services": ["Adobe Creative
 jsonTaskManagerUnusedReq = {"sender": "task-manager", "services": ["Adobe Creative Cloud", "Office 365"], "requested": "Unused"}
 jsonTaskManagerNeededReq = {"sender": "task-manager", "services": ["Adobe Creative Cloud", "Office 365"], "requested": "Needed"}
 
+jsonReq = {"sender": "", "services": [], "requested": ""}
+subTrackerData = ["Netflix", "Duolingo", "Dashpass", "Adobe Creative Cloud"]
+taskManagerData = ["Adobe Creative Cloud", "Office 365"]
+
+# method A: assigning values to JSON template
+print()
+print("Method A: assigning values to JSON template")
+print("############################################")
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "subscription-tracker"
+jsonReq["services"] = subTrackerData
+jsonReq["requested"] = "Used"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Used subscriptions/services: ")
+print(message)
+print()
+UsedSubscriptions = message["subscriptions"]
+
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "task-manager"
+jsonReq["services"] = taskManagerData
+jsonReq["requested"] = "Used"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Used subscriptions/services.")
+print(message)
+print()
+UsedSubscriptions = message["subscriptions"]
+
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "subscription-tracker"
+jsonReq["services"] = subTrackerData
+jsonReq["requested"] = "Unused"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Unused subscriptions/services: ")
+print(message)
+print()
+UnusedSubscriptions = message["subscriptions"]
+
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "task-manager"
+jsonReq["services"] = taskManagerData
+jsonReq["requested"] = "Unused"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Unused subscriptions/services.")
+print(message)
+print()
+UnusedSubscriptions = message["subscriptions"]
+
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "subscription-tracker"
+jsonReq["services"] = subTrackerData
+jsonReq["requested"] = "Needed"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Needed subscriptions/services: ")
+print(message)
+print()
+NeededSubscriptions = message["subscriptions"]
+
+print("Sending json made with assigned values to template.")
+jsonReq["sender"] = "task-manager"
+jsonReq["services"] = taskManagerData
+jsonReq["requested"] = "Needed"
+# print(jsonReq)
+socket.send_json(jsonReq)
+message = socket.recv_json()
+print("Received reply - Needed subscriptions/services.")
+print(message)
+print()
+NeededSubscriptions = message["subscriptions"]
+
+# method B: sending custom typed JSON
+print()
+print("Method B: sending custom typed JSON")
+print("############################################")
+print("Sending custom typed JSON.")
 socket.send_json(jsonSubTrackerUsedReq)
 message = socket.recv_json()
 print("Received reply as SubTracker - Used subscriptions/services: ")
@@ -24,6 +109,7 @@ print(message)
 print()
 UsedSubscriptions = message["subscriptions"]
 
+print("Sending custom typed JSON.")
 socket.send_json(jsonTaskManagerUsedReq)
 message = socket.recv_json()
 print("Received reply as TaskManager - Used subscriptions/services: ")
@@ -31,6 +117,7 @@ print(message)
 print()
 UsedSubscriptions = message["subscriptions"]
 
+print("Sending custom typed JSON.")
 socket.send_json(jsonSubTrackerUnusedReq)
 message = socket.recv_json()
 print("Received reply as SubTracker - Unused subscriptions/services: ")
@@ -38,6 +125,7 @@ print(message)
 print()
 UnusedSubscriptions = message["subscriptions"]
 
+print("Sending custom typed JSON.")
 socket.send_json(jsonTaskManagerUnusedReq)
 message = socket.recv_json()
 print("Received reply TaskManager - Unused subscriptions/services: ")
@@ -45,7 +133,7 @@ print(message)
 print()
 UnusedSubscriptions = message["subscriptions"]
 
-
+print("Sending custom typed JSON.")
 socket.send_json(jsonSubTrackerNeededReq)
 message = socket.recv_json()
 print("Received reply as SubTracker - Needed subscriptions/services: ")
@@ -53,6 +141,7 @@ print(message)
 print()
 NeededSubscriptions = message["subscriptions"]
 
+print("Sending custom typed JSON.")
 socket.send_json(jsonTaskManagerNeededReq)
 message = socket.recv_json()
 print("Received reply TaskManager - Needed subscriptions/services: ")
@@ -66,3 +155,4 @@ print("UnusedSubscriptions: ")
 print(UnusedSubscriptions)
 print("NeededSubscriptions: ")
 print(NeededSubscriptions)
+
